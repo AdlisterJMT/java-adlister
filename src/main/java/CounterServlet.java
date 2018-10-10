1f012f871e442ed8aae0ed5f68c0ee16d0e8dfc0
tamisharutledge <tamisha.rutledge@gmail.com>
2018-10-10 14:52:02 -0500

    Counter page.

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int counter = 0;
+    //    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        boolean reset = Boolean.valueOf(request.getParameter("reset"));
+
+        if (reset){
+            counter = 0;
+        }
+
+        counter += 1;
+        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+
+
+
+    }
+}
+
