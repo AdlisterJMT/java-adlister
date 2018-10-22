7bfd51b72d6a2b7b507028f362ad62b24bc11fec
MichaelWaymire <mike.waymire2@gmail.com>
2018-10-22 11:13:48 -0500

    added error message to login-error jsp

---
+++ src/main/java/com/codeup/adlister/dao/Config.java
@@ -0,0 +1,28 @@
+package com.codeup.adlister.dao;
+
+public class Config {
+    private String url;
+    private String user;
+    private String password;
+
+    public String getUrl() {
+        if(url == null){
+            url = "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";
+        }
+        return url;
+    }
+    public String getUser() {
+        if(user == null){
+            user = "codyTheDuck";
+        }
+        return user;
+    }
+    public String getPassword() {
+        if(password == null){
+            password = "cody";
+        }
+        return password;
+    }
+
+
+}
