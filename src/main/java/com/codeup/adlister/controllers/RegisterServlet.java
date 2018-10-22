695a4413053ac8de082922bbadf4cf786b291be7
MichaelWaymire <mike.waymire2@gmail.com>
2018-10-22 09:41:24 -0500

    Created Login error jsp and servlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -2,12 +2,18 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import com.github.ankurpathak.password.bean.constraints.ContainDigit;
+import com.github.ankurpathak.password.bean.constraints.ContainSpecial;
+import com.github.ankurpathak.password.bean.constraints.NotContainWhitespace;
+import com.github.ankurpathak.password.bean.constraints.PasswordMatches;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
@@ -16,13 +22,16 @@ public class RegisterServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
-        // validate input
+
+
+
         boolean inputHasErrors = username.isEmpty()
             || email.isEmpty()
             || password.isEmpty()
